eb5080f63f8c393721fa99a2e708b59f2ccc55f0
juan arroyo <juaarroyo@hotmail.com>
2020-12-10 19:20:03 -0300

    Correciones

--- src/components/Finanzas/ReporteEgresos/ReporteEgresos.js
+++ src/components/Finanzas/ReporteEgresos/ReporteEgresos.js
@@ -1,41 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Row, Table } from 'react-bootstrap';
-import {Layout} from '../LayoutFinanzas';
+import {Layout, LayoutCrud} from '../LayoutFinanzas';
+import CrudTable from '../../Comunes/CrudTable';
+import { apiSetStateFromUrl } from '../../Comunes/Api';
+import { CustomSpinner } from '../../Comunes/CustomSpinner';
 
+
+const HEADER=['Total egresos del mes']
 const ReporteEgresos =() =>{
-    return(
-    <Layout>
-        <h1>Egresos del último mes</h1>
-        <br></br>
-    <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Egresos</th>
-      <th>Descripción</th>
-      <th>Fecha</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>Otto</td>
-      
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>Otto</td>
-      
-    </tr>
     
-  </tbody>
-</Table>
-</Layout>
+  const [egresos, setEgresos] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+
+    apiSetStateFromUrl("/api/movimientos/totalegresosmes", setEgresos, setLoading);
+    
+    document.title = 'Admin Egresos Mensuales';
+}, [])
+  
+  
+  return(
+  <Layout>
+  <LayoutCrud>
+  {!loading ? <CrudTable items={egresos} setItems={setEgresos} header={HEADER} title="Egresos del ultimo mes"  url="/api/movimientos/totalegresosmes" nameId="totalEgresosMes" apiSetStateFromUrl={apiSetStateFromUrl}  eliminar={false} editar={false} agregar={false}  /> : <CustomSpinner />}
+  </LayoutCrud>
+  </Layout>
 )
 }
 export default ReporteEgresos;
--- src/components/Finanzas/ReporteIngreso/ReporteIngreso.js
+++ src/components/Finanzas/ReporteIngreso/ReporteIngreso.js
@@ -1,41 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Row, Table } from 'react-bootstrap';
-import {Layout} from '../LayoutFinanzas';
+import {Layout, LayoutCrud} from '../LayoutFinanzas';
+import CrudTable from '../../Comunes/CrudTable';
+import { apiSetStateFromUrl } from '../../Comunes/Api';
+import { CustomSpinner } from '../../Comunes/CustomSpinner';
 
-const ReporteIngresos =() =>{
-    return(
-    <Layout>
-        <h1>Ingresos del último mes</h1>
-        <br></br>
-    <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Ingresos</th>
-      <th>Descripción</th>
-      <th>Fecha</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>Otto</td>
-      
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>Otto</td>
-      
-    </tr>
+
+const HEADER=['Total Ingresos del mes']
+const ReporteEgresos =() =>{
     
-  </tbody>
-</Table>
-</Layout>
+  const [ingresos, setIngresos] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+
+    apiSetStateFromUrl("/api/movimientos/totalingresosmes", setIngresos, setLoading);
+    
+    document.title = 'Admin Ingresos Mensuales';
+}, [])
+  
+  
+  return(
+  <Layout>
+  <LayoutCrud>
+  {!loading ? <CrudTable items={ingresos} setItems={setIngresos} header={HEADER} title="Ingresos del ultimo mes"  url="/api/movimientos/totalingresosmes" nameId="totalIngresosMes" apiSetStateFromUrl={apiSetStateFromUrl}  eliminar={false} editar={false} agregar={false}  /> : <CustomSpinner />}
+  </LayoutCrud>
+  </Layout>
 )
 }
-export default ReporteIngresos;
+export default ReporteEgresos;
