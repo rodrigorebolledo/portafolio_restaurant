ffe607978a09c3c5b034a46563f4e2823ff13e18
juan arroyo <juaarroyo@hotmail.com>
2020-12-10 02:22:36 -0300

    Reporte funcional con grafico de platos vendidos

--- src/components/Finanzas/ReportePlatos/ReportePlatos.js
+++ src/components/Finanzas/ReportePlatos/ReportePlatos.js
@@ -1,38 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Row, Table } from 'react-bootstrap';
+import { Bar } from 'react-chartjs-2';
+import axios from 'axios';
 import Layout from '../LayoutFinanzas';
 
+
 const ReportePlatos =() =>{
+    const[cantidadPlato, setCantidadPlato]=useState([]);
+    const[nombrePlato, setNombrePlato]=useState([]);
+
+    const data={
+        labels: nombrePlato,
+        datasets:[{
+            label:'Cantidad',
+            backgroundColor:'rgb(255,0,0,1)',
+            borderColor: 'black',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,0,0,0.2)',
+            hoverBorderColor: '#FFFF00',
+            data: cantidadPlato,
+
+        }]
+    };
+    const opciones={
+        maintainAspectRatio: false,
+        responsive: true
+    }
+    const peticionesApi=async()=>{
+        await axios.get('http://localhost:80/api/platos/reporteplatos')
+        .then(response=>{
+            var respuesta=response.data;
+            var auxCantidad=[], auxPlatos=[];
+            respuesta.map(elemento=>{
+               auxCantidad.push(elemento.cantidadPlato);
+               auxPlatos.push(elemento.nombrePlato);
+            });
+            setCantidadPlato(auxCantidad);
+            setNombrePlato(auxPlatos);
+        })
+    }
+
+    useEffect(()=>{
+        console.log(peticionesApi);
+        peticionesApi();
+        
+    },[])
     return(
     <Layout>
-        <h1>Cantidad de platos vendidos del último mes</h1>
-        <br></br>
-    <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Nombre Plato</th>
-      <th>Cantidad Vendidos</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      
-    </tr>
-    
-  </tbody>
-</Table>
+      <div className= "ReportePlatos" style={{width: "100%", height: "500px"}}>
+          <h2>Cantidad de Platos vendidos</h2>
+          <Bar data={data} options={opciones}></Bar>
+
+      </div>
 </Layout>
-)
+);
 }
 export default ReportePlatos;
